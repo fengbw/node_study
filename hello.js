454534d00175e8d68763b14248e67cf26dbdd324
fengbw <fengbwseu@163.com>
2020-10-26 20:34:15 -0400

    style

--- hello.js
+++ hello.js
@@ -1,7 +1,9 @@
 const http = require('http')
 
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Context-Type': 'text/plain'})
+    response.writeHead(200, {
+        'Context-Type': 'text/plain'
+    })
 
     response.end('Hello world')
 }).listen(8888, () => {
